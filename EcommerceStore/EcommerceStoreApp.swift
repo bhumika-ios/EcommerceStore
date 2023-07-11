d71ef2e8dcd888ea8b2047818520451a5bcbe1d7
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-07-11 16:31:22 +0530

    Initial Commit

---
+++ EcommerceStore/EcommerceStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceStoreApp.swift
+//  EcommerceStore
+//
+//  Created by Bhumika Patel on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
