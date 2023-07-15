8140dba1b9a682efa8019fd570a9b7f85d737353
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-07-15 17:27:45 +0530

    create loading view for loading process

---
+++ EcommerceStore/View/ProductViews/LoadingView.swift
@@ -0,0 +1,46 @@
+//
+//  LoadingView.swift
+//  EcommerceStore
+//
+//  Created by Bhumika Patel on 11/07/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    let isLoading: Bool
+    let error: NSError?
+    let retryAction: (() -> ())?
+    var body: some View {
+        Group {
+            if isLoading{
+                VStack {
+                    Spacer()
+                    HStack{
+                        Spacer()
+                   //     ProductLoading()
+                            .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+            } else if error != nil {
+                Text("Can't load the products")
+                if retryAction != nil {
+                    Button(action: retryAction!){
+                        Text("retry")
+                    }
+                }
+            } else {
+                EmptyView()
+            }
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView(isLoading: true, error: nil, retryAction: nil)
+    }
+}
+
